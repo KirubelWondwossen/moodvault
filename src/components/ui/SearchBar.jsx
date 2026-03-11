cac228f32ab76ac0f631e5b2e6245e93e913c167
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2026-03-11 18:37:56 +0300

    feat(search): implement search functionality

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,13 +1,43 @@
 import { Search } from "lucide-react";
+import SearchResult from "./SearchResult";
+import { useQuery } from "@tanstack/react-query";
+import useDebounce from "../hooks/useDebounce";
+import { searchAll } from "../../services/search";
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debouncedSearch = useDebounce(searchTerm, 400);
+
+  const searchQuery = useQuery({
+    queryKey: ["search", debouncedSearch],
+    queryFn: () => searchAll(debouncedSearch),
+    enabled: !!debouncedSearch,
+  });
+
+  const results = debouncedSearch && searchQuery.data;
+  const isLoading = debouncedSearch && searchQuery.isLoading;
+
   return (
-    <div className="flex items-center px-5 relative text-tPrimary">
-      <Search className="w-5 left-[10%] absolute cursor-pointer text-[#ffffffde]" />
+    <div className="relative px-5 w-full">
+      <SearchBtn searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <SearchResult results={results} isLoading={isLoading} />
+    </div>
+  );
+}
+
+function SearchBtn({ searchTerm, setSearchTerm }) {
+  return (
+    <div className="flex items-center relative text-tPrimary">
+      <Search className="w-5 left-3 absolute cursor-pointer text-[#ffffffde]" />
+
       <input
         type="search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
         className="text-heading pl-9 p-2 focus:outline-none w-64 bg-background border border-borderColor rounded-xl"
-        placeholder="Search movies..."
+        placeholder="Search movies, anime, series..."
       />
     </div>
   );
