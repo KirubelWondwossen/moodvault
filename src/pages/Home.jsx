1f0d39a1b6cb424620b650f4ceec34feb477187d
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2026-03-27 22:00:55 +0300

    feat(UI): add the trending now section for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries, useQuery } from "@tanstack/react-query";
 import MainLayout from "../components/Layout/MainLayout";
 import { Tags } from "../components/ui/Tags";
 import { useAuth } from "../context/AuthContext";
@@ -6,6 +6,9 @@ import { fetchUserItems } from "../lib/items";
 import CardContainer from "../components/Layout/CardContainer";
 import { sortByLatest } from "../utils/filterOptions";
 import { SkeletonGrid } from "../components/ui/SkeletonGrid";
+import { getTrendingMovies, getTrendingTv } from "../services/tmdb";
+import { getSeasonalAnime } from "../services/jikan";
+import { useAllMedia } from "../hooks/useAllMedia";
 
 export default function Home() {
   const { user } = useAuth();
@@ -14,8 +17,22 @@ export default function Home() {
     queryFn: () => fetchUserItems(user.uid),
   });
 
-  const latestItems = data ? sortByLatest(data) : [];
+  const results = useQueries({
+    queries: [
+      { queryKey: ["trendingMovies"], queryFn: getTrendingMovies },
+      { queryKey: ["currentAnime"], queryFn: getSeasonalAnime },
+      { queryKey: ["trendingTv"], queryFn: getTrendingTv },
+    ],
+  });
+  const [trendingMovieQuery, currentAnimeQuery, trendingTvQuery] = results;
 
+  const trending = useAllMedia(
+    trendingMovieQuery,
+    trendingTvQuery,
+    currentAnimeQuery,
+  );
+
+  const latestItems = data ? sortByLatest(data) : [];
   return (
     <MainLayout title={`Welcome, ${user.firstName}`}>
       {isLoading && <SkeletonGrid count={12} />}
@@ -23,10 +40,16 @@ export default function Home() {
         <>
           <MoodPicker />
           <CardContainer
+            data={trending}
+            title={"Trending Now"}
+            link={"/explore"}
+            className={"mt-4"}
+          />
+          <CardContainer
             data={latestItems}
             title={"Continue Watching (from My Vault)"}
             link={"/myvault"}
-            className={"mt-4"}
+            className={"mt-4 mb-5"}
           />
         </>
       )}
